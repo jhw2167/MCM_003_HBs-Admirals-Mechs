4de807f4aed99bb2ef7c1de7432e9dbe896e33af
jhw2167 <jhw2167@columbia.edu>
2025-07-24 15:52:17 -0500

    - initial commit, copied template dir

---
+++ common/src/main/java/com/holybuckets/admiralsmechs/Constants.java
@@ -0,0 +1,11 @@
+package com.holybuckets.admiralsmechs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "hbs_admirals_mechs";
+	public static final String MOD_NAME = "HBs Admirals Mechs";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ common/src/main/java/com/holybuckets/admiralsmechs/item/TemplateBlockItem.java
@@ -0,0 +1,13 @@
+package com.holybuckets.admiralsmechs.item;
+
+import com.holybuckets.admiralsmechs.block.ModBlocks;
+import net.minecraft.world.item.BlockItem;
+
+public class TemplateBlockItem extends BlockItem {
+
+    public TemplateBlockItem(Properties properties) {
+        super(ModBlocks.templateBlock, properties);
+    }
+
+
+}
